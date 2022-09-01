b7cc2ea1a5ebddf9ac34f347c3b6bf83af6f863c
S <30459885+tacticstactics@users.noreply.github.com>
2022-09-01 16:05:59 +0900

    2

--- PythonApplication1/PythonApplication1/mach_zender_interferometer_def.py
+++ PythonApplication1/PythonApplication1/mach_zender_interferometer_def.py
@@ -1,22 +1,27 @@
 #mach_zender_interferometer_def.py
 
+import math
 import numpy as np
 
-def proc1(param=0.01,m=512):
+
+def propagate1(wavel=1.55e-6,no=1,opl=1,Ein=np.array([[1],[0]])):
+
+    T11 = np.array([[np.exp(1j*wavel*no*opl),0],[0,np.exp(1j*wavel*no*opl)]]);
+
+    Eout = np.dot(T11,Ein)
+
+    return Eout
 
 
-    rscol = np.zeros((m,1))
+
+def beamsplitter(PT=0.5,Ein=np.array([[1],[0]])):
+
+     PR = 1-PT
+
+     Theta1 = np.arctan(PR/PT) #Radian
 
 
-    return rscol
-
-
-
-def beamsplitter(T=0.5,Ein=np.array([[1],[0]])):
-
-     R=1-T
-
-     BS1 = np.array([[R],[1]],[[R],[1]])
+     BS1 = np.array([[math.sin(Theta1),math.cos(Theta1)],[math.cos(Theta1),-1*math.sin(Theta1)]])
        
      Eout=np.dot(BS1,Ein)
 
--- PythonApplication1/PythonApplication1/mach_zender_interferometer_main.py
+++ PythonApplication1/PythonApplication1/mach_zender_interferometer_main.py
@@ -5,16 +5,35 @@ import matplotlib.pyplot as plt
 
 import mach_zender_interferometer_def
 
-param=0.1
-m=256
-
 print('')
 print('mach-zender_interferometer_main.py')
 print('')
 
 
-rscol = mach_zender_interferometer_def.proc1(param,m)
+wavel = 1.55e-6
+no = 1
+ne = 1.1
+theta1 = 22.5
+opl = 9999
 
 
-fig = plt.figure(figsize = (10,6), facecolor='lightblue')
+Ein1 = np.array([[1],[0]])
+
+Eout1 = mach_zender_interferometer_def.propagate1(wavel, no, opl, Ein1)
+
+
+PT = 0.9
+
+Eout2 = mach_zender_interferometer_def.beamsplitter(PT, Eout1)
+
+print('')
+
+print('E2')
+print(Eout2)
+
+print('')
+
+#fig = plt.figure(figsize = (10,6), facecolor='lightblue')
+
+
 
