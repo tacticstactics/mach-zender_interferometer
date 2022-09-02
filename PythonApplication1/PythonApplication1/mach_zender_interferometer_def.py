a635b6d98fa4915240004b712b32fd21c8d7e9be
S <30459885+tacticstactics@users.noreply.github.com>
2022-09-02 17:48:07 +0900

    1

--- PythonApplication1/PythonApplication1/mach_zender_interferometer_def.py
+++ PythonApplication1/PythonApplication1/mach_zender_interferometer_def.py
@@ -4,52 +4,29 @@ import math
 import numpy as np
 
 
-def propagate1(wavel=1.55e-6,no=1,opl=1,Ein=np.array([[1],[0]])):
 
-    T11 = np.array([[np.exp(1j*wavel*no*opl),0],[0,np.exp(1j*wavel*no*opl)]]);
+def propagate1(wavel=1.55e-6,no=1, opl1=1, opl2=1, Ein=np.array([[1],[0]])):
 
-    Eout = np.dot(T11,Ein)
+    propagatematrix1 = np.array([[np.exp(1j*wavel*no*opl1),0],[0,np.exp(1j*wavel*no*opl2)]]);
+
+    Eout = np.dot(propagatematrix1,Ein)
+
+    #Pout = np.array([[],[]])
+
 
     return Eout
 
 
 
-def dielectric_beamsplitter(PT=0.5,Ein=np.array([[1],[0]])):
+def dielectric_beamsplitter(PT,Ein):
 
      phiT = 0
-     PhiR = 0
+     phiR = 0
      phiO = 0
 
-
-     T = np.sqrt(PT)
-
-     PR = 1-PT
-
-     R = np.sqrt(PR)
-
-     Theta1 = np.arctan(R/T) #Radian
-     
-     print('')
-
-     print('Theta1 = ')
-     print(Theta1)
-
-     print('')
-     
-     # https://en.wikipedia.org/wiki/Beam_splitter
-
-     deBS1 = np.array([[math.sin(Theta1),math.cos(Theta1)],[math.cos(Theta1),-1*math.sin(Theta1)]])
-       
-     Eout = np.dot(deBS1,Ein)
-
-     return Eout
-
-
- def Loudon_beamsplitter(PT=0.5,Ein=np.array([[1],[0]])):
-
-     phiT = 0
-     PhiR = -0.5*np.pi
-     phiO = 0.5 * np.pi
+     #phiT = 0
+     #phiR = -0.5*np.pi
+     #phiO = 0.5 * np.pi
 
 
      T = np.sqrt(PT)
@@ -61,7 +38,6 @@ def dielectric_beamsplitter(PT=0.5,Ein=np.array([[1],[0]])):
      Theta1 = np.arctan(R/T) #Radian
      
      print('')
-
      print('Theta1 = ')
      print(Theta1)
 
@@ -69,8 +45,14 @@ def dielectric_beamsplitter(PT=0.5,Ein=np.array([[1],[0]])):
      
      # https://en.wikipedia.org/wiki/Beam_splitter
 
-     LoudonBS1 = np.array([[math.sin(Theta1),math.cos(Theta1)],[math.cos(Theta1),-1*math.sin(Theta1)]])
-       
-     Eout=np.dot(LoudonBS1,Ein)
+     
+     dielectricBS1 = np.dot(np.exp(-1J * phiO),np.array([[math.sin(Theta1)*np.exp(1J * phiR),math.cos(Theta1)*np.exp(-1J * phiT)],[math.cos(Theta1)*np.exp(1j * phiT),-1*math.sin(Theta1)*np.exp(-1J * phiR)]]))
+     
+     print('')
+     print('dielectricBS1 = ')
+     print(dielectricBS1)
+
+
+     Eout = np.dot(dielectricBS1,Ein)
 
      return Eout
--- PythonApplication1/PythonApplication1/mach_zender_interferometer_main.py
+++ PythonApplication1/PythonApplication1/mach_zender_interferometer_main.py
@@ -2,7 +2,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 import mach_zender_interferometer_def
 
 print('')
@@ -14,24 +13,67 @@ wavel = 1.55e-6
 no = 1
 #ne = 1.1
 #theta1 = 22.5
-opl = 9999
+oplcommon1=1
+oplcommon2=1
 
+opl1 = 400
+opl2= 399.99
 
 Ein1 = np.array([[1],[0]])
 
-Eout1 = mach_zender_interferometer_def.propagate1(wavel, no, opl, Ein1)
+#Ein1 = np.array([[0.707],[0.707]])
 
+
+print('')
+print('E1')
+print(Ein1)
+print('')
+
+
+Eout1 = mach_zender_interferometer_def.propagate1(wavel, no, oplcommon1, oplcommon2, Ein1)
+
+Ein2 = Eout1
+
+print('')
+print('E2')
+print(Ein2)
+print('')
 
 PT = 0.5
 
-Eout2 = mach_zender_interferometer_def.dielectric_beamsplitter(PT, Eout1)
+#PT = 0.25
+#PT = 0.999
+
+Eout2 = mach_zender_interferometer_def.dielectric_beamsplitter(PT, Ein2)
 
 print('')
-
 print('E2')
 print(Eout2)
+print('')
+
+
+Ein3 = Eout2
+Eout3 = mach_zender_interferometer_def.propagate1(wavel, no, opl1, opl2, Ein3)
 
 print('')
+print('Eout3')
+print(Eout3)
+print('')
+
+
+
+Ein4 = Eout3
+Eout4 = mach_zender_interferometer_def.dielectric_beamsplitter(PT, Ein4)
+
+
+Ein5 = Eout4
+Eout5 = mach_zender_interferometer_def.propagate1(wavel, no, oplcommon1, oplcommon2, Ein5)
+
+print('')
+print('Eout5')
+print(Eout5)
+print('')
+
 
 #fig = plt.figure(figsize = (10,6), facecolor='lightblue')
 
