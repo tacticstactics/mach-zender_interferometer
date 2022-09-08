6b2277054a71aa1fd0b46f9307080edbf7a1e271
S <30459885+tacticstactics@users.noreply.github.com>
2022-09-08 09:17:24 +0900

    1

--- PythonApplication1/PythonApplication1/mach_zender_interferometer_def.py
+++ PythonApplication1/PythonApplication1/mach_zender_interferometer_def.py
@@ -4,7 +4,6 @@ import math
 import numpy as np
 
 
-
 def propagate1(wl=0.633,no=1, opl1=1, opl2=1, Ein=np.array([[1],[0]])):
 
     propagatematrix1 = np.array([[np.exp(1j*wl*no*opl1),0],[0,np.exp(1j*wl*no*opl2)]]);
@@ -33,9 +32,9 @@ def beamsplitter(PT,Ein):
      phiO = 0.5 * np.pi
 
 
-     T = np.sqrt(PT)
+     T = np.sqrt(PT) # Transmission as Electric field
 
-     PR = 1-PT
+     PR = 1-PT # Reflection as Electric field
 
      R = np.sqrt(PR)
 
@@ -43,7 +42,6 @@ def beamsplitter(PT,Ein):
          
      dielectricBS1 = np.dot(np.exp(1J*phiO),np.array([[math.sin(Theta1)*np.exp(1J*phiR),math.cos(Theta1)*np.exp(-1J*phiT)],[math.cos(Theta1)*np.exp(1j*phiT),-1*math.sin(Theta1)*np.exp(-1J*phiR)]]))
      
-
      Eout = np.dot(dielectricBS1,Ein)
 
      return Eout
--- PythonApplication1/PythonApplication1/mach_zender_interferometer_main.py
+++ PythonApplication1/PythonApplication1/mach_zender_interferometer_main.py
@@ -12,28 +12,28 @@ print('')
 
 m = 512
 
-no = 1
-#ne = 1.1
+no = 1 # Refractive Index of medium
 
 oplcommon1=100
 oplcommon2=100
 
-opl1 = 10
+opl1 = 1 
 opl2= 1000
+# Optical Path Length Difference (opl-opl1) determines free spectral range.
 
 wl0 = 0.633; #um
 stepwl = 0.00003; # um
 
-PT1 = 0.5 # PT: Power Transmission
-PT2 = 0.49 # PT: Power Transmission
+PT1 = 0.75 # PT: Power Transmission of first beam splitter
+PT2 = 0.25 # PT: Power Transmission of second beam splitter
 
 # Input Port 1 only
 #Ein1 = np.array([[1+0.0000j],[0-0.0000j]])
 #Ein1 = np.array([[0.707+0.707j],[0]])
 
 # Input Both 1 and 2 port
-Ein1 = np.array([[1+0j],[1-0j]]) #Both 1 and 2 port
-#Ein1 = np.array([[0.707+0.707j],[-0.707-0.707j]]) #Both 1 and 2 port
+#Ein1 = np.array([[1+0j],[1-0j]]) #Both 1 and 2 port
+Ein1 = np.array([[0.707+0.707j],[-0.707-0.707j]]) #Both 1 and 2 port
 
 # Input Port 2 only
 #Ein1 = np.array([[0],[1]]) 
@@ -45,7 +45,6 @@ P1_powercol = np.zeros((m,1));
 P1_phasecol = np.zeros((m,1));
 
 P2_powercol = np.zeros((m,1));
-PP2col = np.zeros((m,1));
 P2_phasecol = np.zeros((m,1));
 
 
@@ -69,8 +68,7 @@ for ii in range(m):
  Eout5 = mach_zender_interferometer_def.propagate1(wl, no, oplcommon1, oplcommon2, Ein5)
  Ein6 = Eout5
  
- Eout_port1 = Ein6[0,0]
-  
+ Eout_port1 = Ein6[0,0] 
  power_11 = (np.abs(Eout_port1))**2
  P1_powercol[(ii)] = power_11
  
@@ -79,7 +77,7 @@ for ii in range(m):
  
  Eout_port_2 = Ein6[1,0]
  power_22 = (np.abs(Eout_port_2))**2
- PP2col[(ii)] = power_22
+ 
  P2_powercol[(ii)] = power_22
 
  P2_phase = cmath.phase(power_22)
@@ -87,28 +85,22 @@ for ii in range(m):
  
  
 
-fig = plt.figure(figsize = (10,6), facecolor='lightblue')
+fig = plt.figure(figsize = (10,4), facecolor='lightblue')
 
-ax1 = fig.add_subplot(2, 2, 1)
-ax2 = fig.add_subplot(2, 2, 2, sharey=ax1)
-ax3 = fig.add_subplot(2, 2, 3)
-ax4 = fig.add_subplot(2, 2, 4, sharey=ax3)
+ax1 = fig.add_subplot(1, 2, 1)
+ax2 = fig.add_subplot(1, 2, 2)
 
 ax1.plot(wlcol,P1_powercol,wlcol,P2_powercol)
-
+ax1.set_xlabel("Wavelength")
 ax1.set_ylabel("Power")
 ax1.set_ylim(0,2)
 ax1.grid()
 
-ax3.plot(wlcol,P1_phasecol,wlcol,P2_phasecol)
-ax4.plot()
-
-
-
-ax3.set_xlabel("Wavelength")
-ax4.set_xlabel("Wavelength")
-ax3.set_ylabel("Phase")
-
+ax2.plot(wlcol,P1_phasecol,wlcol,P2_phasecol)
+ax2.set_xlabel("Wavelength")
+ax2.set_ylabel("Phase")
+ax2.set_ylim(-2,2)
+ax2.grid()
 
 plt.show()
 
