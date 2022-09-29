563d062f61f246e44fccf073548d51d99e00624c
S <30459885+tacticstactics@users.noreply.github.com>
2022-09-29 15:24:19 +0900

    1

--- PythonApplication1/mach_zender_interferometer_wavelgnth_main.py
+++ PythonApplication1/mach_zender_interferometer_wavelgnth_main.py
@@ -42,19 +42,19 @@ Ein1 = np.array([[0.707+0.707j],[-0.707-0.707j]])
 #Ein1 = np.array([[0],[1]]) 
 #Ein1 = np.array([[0],[0.707+0.707j]])
 
-wlcol = np.zeros((m,1));
+wlcol = np.zeros(m);
 
-P1_powercol = np.zeros((m,1));
-P1_phasecol = np.zeros((m,1));
+P1_powercol = np.zeros(m);
+P1_phasecol = np.zeros(m);
 
-P2_powercol = np.zeros((m,1));
-P2_phasecol = np.zeros((m,1));
+P2_powercol = np.zeros(m);
+P2_phasecol = np.zeros(m);
 
 
 
 for ii in range(m):
  wl = wl0 + ii * stepwl
- wlcol[(ii)] =wl
+ wlcol[ii] =wl
 
  Eout1 = mach_zender_interferometer_def.propagate1(wl, no, oplcommon1, oplcommon2, Ein1)
  Ein2 = Eout1
@@ -73,18 +73,18 @@ for ii in range(m):
  
  Eout_port1 = Ein6[0,0] 
  power_11 = (np.abs(Eout_port1))**2 # Optical power is calculated as square of absolute electric field strength
- P1_powercol[(ii)] = power_11
+ P1_powercol[ii] = power_11
  
  P1_phase = cmath.phase(power_11)
- P1_phasecol[(ii)] = P1_phase
+ P1_phasecol[ii] = P1_phase
  
  Eout_port_2 = Ein6[1,0]
  power_22 = (np.abs(Eout_port_2))**2
  
- P2_powercol[(ii)] = power_22
+ P2_powercol[ii] = power_22
 
  P2_phase = cmath.phase(power_22)
- P2_phasecol[(ii)] = P2_phase
+ P2_phasecol[ii] = P2_phase
  
  
 
