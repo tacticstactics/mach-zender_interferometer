e2e0a2f64c183ac5e74bccbd9a10ca7d4decce5a
S <30459885+tacticstactics@users.noreply.github.com>
2023-03-17 11:40:21 +0900

    1

--- time/mach_zender_interferometer_IQ modulation_main.py
+++ time/mach_zender_interferometer_IQ modulation_main.py
@@ -8,7 +8,6 @@ print('')
 print('mach-zender_interferometer_IQ modulation_main.py')
 print('')
 
-
 wl = 0.633; #wavelength in um
 
 samplerate = 2048 # Sampling Frequency. [Hz]
@@ -16,15 +15,16 @@ stept = 1/samplerate
 
 amp_c1 = 2.5
 freq_am1 = 5 # [Hz]
-md = 1 # modulation depth. 1 = 100 %
-dc_offset1 = 2.1 # DC offset
+dc_offset1 = 2.5 # DC offset
 
 
 amp_c2 = 2.5
 freq_am2 = 5 # [Hz]
-md = 1 # modulation depth. 1 = 100 %
-dc_offset2 = 2.1 # DC offset
+dc_offset2 = 2.5 # DC offset
 
+phase_IQ = 1*np.pi
+
+IPB = 300 #In Phase Bias
 
 
 no = 1 # Refractive Index of medium
@@ -38,21 +38,21 @@ opl2= 100
 # Optical Path Length Difference (opl1-opl2) determines free spectral range as optical filter.
 
 
+
 PT1 = 0.5 # PT: Power Transmission of first beam splitter
 
-PT2_1 = 0.5 # PT: Power Transmission of second beam splitter
-PT2_2 = 0.5 # PT: Power Transmission of second beam splitter
+PT2_1 = 0.5 # PT: Power Transmission of second beam splitter of arm A
+PT2_2 = 0.5 # PT: Power Transmission of second beam splitter of arm B
 
-PT3_1 = 0.5 # PT: Power Transmission of second beam splitter
-PT3_2 = 0.5 # PT: Power Transmission of second beam splitter
+PT3_1 = 0.5 # PT: Power Transmission of third beam splitter of arm A
+PT3_2 = 0.5 # PT: Power Transmission of third beam splitter of arm B
 
-PT4 = 0.5 # PT: Power Transmission of first beam splitter
+PT4 = 0.5 # PT: Power Transmission of 4th beam splitter
 
 # Define Input Electric Field
 
 # Input: Port 1 only
 E1in = np.array([[1+0.0000j],[0-0.0000j]])
-print(E1in)
 #Ein1 = np.array([[0.707+0.707j],[0]])
 
 # Input Both 1 and 2 port
@@ -79,7 +79,6 @@ for ii in range(samplerate):
     
     t = stept * ii
     tcol[ii] = t
-
     
     E1out = mach_zender_interferometer_time_def.propagate1(wl, no, oplcommon1, oplcommon2, E1in)
     E2in = E1out
@@ -87,6 +86,7 @@ for ii in range(samplerate):
     E2out = mach_zender_interferometer_time_def.beamsplitter(PT1, E2in)
     E3_1in = np.array([[E2out[0,0]],[0+0j]])
 
+    #print("E3_1in")
     #print(E3_1in)
     #print("")
 
@@ -106,40 +106,57 @@ for ii in range(samplerate):
     E4_1out = mach_zender_interferometer_time_def.propagate1(wl, no, opl1, opl2+signal1, E4_1in) # Each path experience different path length
     E5_1in = E4_1out
     
-    E5_1out = mach_zender_interferometer_time_def.beamsplitter(PT2_2, E5_1in) # Each path enter second beam splitter
+    E5_1out = mach_zender_interferometer_time_def.beamsplitter(PT3_1, E5_1in) # Each path enter second beam splitter
     E6_1in = E5_1out
+
+    E6_1out = mach_zender_interferometer_time_def.propagate1(wl, no, 0, 0, E6_1in) # do not experience delay
 
 
     #Arm 2
 
     E3_2in = np.array([[E2out[1,0]],[0+0j]])
-
-    print(E3_2in)
-    print("")
- 
-    signal2 = amp_c2 * np.sin(2 * np.pi * freq_am2 * t + np.pi) + dc_offset2
-    signal2col[ii] = signal2
+    
+    #print("E3_2in")
+    #print(E3_2in)
+    #print("")
 
     E3_2out = mach_zender_interferometer_time_def.beamsplitter(PT2_2, E3_2in)
 
     E4_2in = E3_2out
 
+ 
+    signal2 = amp_c2 * np.sin(2 * np.pi * freq_am2 * t + phase_IQ) + dc_offset2
+    signal2col[ii] = signal2    
     
+
+    E4_2out = mach_zender_interferometer_time_def.propagate1(wl, no, opl1, opl2+signal2, E4_2in) # Each path experience different path length
+    E5_2in = E4_2out
+
+    E5_2out = mach_zender_interferometer_time_def.beamsplitter(PT3_2, E5_2in) # Each path enter second beam splitter
+    E6_2in = E5_2out
+
+    E6_2out = mach_zender_interferometer_time_def.propagate1(wl, no, IPB, IPB, E6_2in) # Actually only one path couple to fourth beam splitter
     
-    #
+    # Combine I + Q using fourth beam splitter
+
+    E7_in = np.array([[E6_1out[0,0]],[E6_2out[0,0]]])
+
+    #print("E7_in")
+    #print(E7_in)
+    #print("")
 
 
-    E6_1out = mach_zender_interferometer_time_def.propagate1(wl, no, oplcommon1, oplcommon2, E6_1in)
-    E7in = E6_1out
+    E7_out = mach_zender_interferometer_time_def.beamsplitter(PT4, E7_in) # Each path enter second beam splitter
+
     
-    Eout_port1 = E7in[0,0] 
+    Eout_port1 = E7_out[0,0] 
     power_11 = (np.abs(Eout_port1))**2 # Optical power is calculated as square of absolute electric field strength
     P1_powercol[ii] = power_11
     
     P1_phase = np.angle(power_11)
     P1_phasecol[ii] = P1_phase
     
-    Eout_port_2 = E7in[1,0]
+    Eout_port_2 = E7_out[1,0]
     power_22 = (np.abs(Eout_port_2))**2
     
     P2_powercol[ii] = power_22
@@ -151,12 +168,13 @@ for ii in range(samplerate):
 
 fig = plt.figure(figsize = (10,6), facecolor='lightblue')
 
-ax1 = fig.add_subplot(3, 1, 1)
-ax2 = fig.add_subplot(3, 1, 2)
-ax3 = fig.add_subplot(3, 1, 3)
+ax1 = fig.add_subplot(2, 1, 1)
+ax2 = fig.add_subplot(2, 1, 2)
 
 ax1.plot(tcol,signal1col, tcol,signal2col)
 #ax1.set_ylim(-3,3)
+ax1.grid()
+
 
 ax2.plot(tcol,P1_powercol,tcol,P2_powercol)
 
@@ -164,11 +182,7 @@ ax2.set_ylabel("Power")
 ax2.set_ylim(0,1.1)
 ax2.grid()
 
-ax3.plot(tcol,P1_phasecol,tcol,P2_phasecol)
-ax3.set_xlabel("time [s]")
-ax3.set_ylabel("Angle")
-ax3.set_ylim(-2,2)
-ax3.grid()
+
 
 plt.show()
 
--- time/mach_zender_interferometer_time_main.py
+++ time/mach_zender_interferometer_time_main.py
@@ -8,14 +8,7 @@ print('')
 print('mach-zender_interferometer_time_main.py')
 print('')
 
-samplerate = 2048 # Sampling Frequency. [Hz]
-
-stept = 1/samplerate
-
-amp_c = 2.5
-freq_am = 5 # [Hz]
-md = 1 # modulation depth. 1 = 100 %
-dc_offset = 2.1 # DC offset
+wl = 0.633; #wavelength in um
 
 no = 1 # Refractive Index of medium
 
@@ -26,25 +19,13 @@ opl1 =100
 opl2= 100
 # Optical Path Length Difference (opl1-opl2) determines free spectral range as optical filter. In the case of modulator, DOPL does not affect.
 
-wl = 0.633; #wavelength in um
-
 PT1 = 0.5 # PT: Power Transmission of first beam splitter
 PT2 = 0.5 # PT: Power Transmission of second beam splitter
 
-# Define Input Electric Field
 
-# Input Port 1 only
-Ein1 = np.array([[1+0.0000j],[0-0.0000j]])
-#Ein1 = np.array([[0.707+0.707j],[0]])
 
-# Input Both 1 and 2 port
-#Ein1 = np.array([[1+0j],[1-0j]]) 
-#Ein1 = np.array([[0.707+0.707j],[-0.707-0.707j]])
-#Ein1 = np.array([[1 + 0j],[-1 - 0j]])
-
-# Input Port 2 only
-#Ein1 = np.array([[0],[1]]) 
-#Ein1 = np.array([[0],[0.707+0.707j]])
+samplerate = 2048 # Sampling Frequency. [Hz]
+stept = 1/samplerate
 
 tcol = np.zeros(samplerate)
 
@@ -52,13 +33,25 @@ sine_signalcol = np.zeros(samplerate)
 random_signalcol = np.zeros(samplerate)
 prbs1 = np.zeros(samplerate)
 
-P1_powercol = np.zeros(samplerate)
-P1_phasecol = np.zeros(samplerate)
+#Sine signal parameters
 
-P2_powercol = np.zeros(samplerate)
-P2_phasecol = np.zeros(samplerate)
+amp_c = 2.5
+freq_am = 5 # [Hz]
+md = 1 # modulation depth. 1 = 100 %
+dc_offset = 2.1 # DC offset
+
+#sinesignal
+for ii in range(samplerate):
+    
+    t = stept * ii
+    tcol[ii] = t
+
+    sinesignal = amp_c * np.sin(2 * np.pi * freq_am * t) + dc_offset
+    sine_signalcol[ii] = sinesignal  
 
 
+
+# Random signal generation
 
 a_range = [1, 10]
 a = np.random.rand(samplerate) * (a_range[1]-a_range[0]) + a_range[0] # range for amplitude
@@ -69,9 +62,6 @@ b = np.round(b)
 b = b.astype(int)
 
 b[0] = 0
-
-# random signal generation
-
 
 for i in range(1,np.size(b)):
     b[i] = b[i-1]+b[i]
@@ -101,19 +91,33 @@ while b[i]<np.size(prbs1):
     i=i+1
 #----
 
-#sinesignal
-for ii in range(samplerate):
-    
-    t = stept * ii
-    tcol[ii] = t
 
-    sinesignal = amp_c * np.sin(2 * np.pi * freq_am * t) + dc_offset
-    sine_signalcol[ii] = sinesignal  
-
-
-#signalcol = random_signal
-signalcol = prbs1
 #signalcol = sine_signalcol
+signalcol = random_signal
+#signalcol = prbs1
+
+
+#Define Input condition
+
+# Input Port 1 only
+Ein1 = np.array([[1+0.0000j],[0-0.0000j]])
+#Ein1 = np.array([[0.707+0.707j],[0]])
+
+# Input Both 1 and 2 port
+#Ein1 = np.array([[1+0j],[1-0j]]) 
+#Ein1 = np.array([[0.707+0.707j],[-0.707-0.707j]])
+#Ein1 = np.array([[1 + 0j],[-1 - 0j]])
+
+# Input Port 2 only
+#Ein1 = np.array([[0],[1]]) 
+#Ein1 = np.array([[0],[0.707+0.707j]])
+
+
+P1_powercol = np.zeros(samplerate)
+P1_phasecol = np.zeros(samplerate)
+
+P2_powercol = np.zeros(samplerate)
+P2_phasecol = np.zeros(samplerate)
 
 
 for ii in range(samplerate):
